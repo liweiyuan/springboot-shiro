1276a73541db3e01702a13b03caa3ab914de5563
535767595@qq.com <lwy19998273333>
2018-03-30 16:25:41 +0800

    springboot-shiro学习

---
+++ src/main/java/com/suishenyun/springbootshiro/controller/AccountInfoController.java
@@ -0,0 +1,27 @@
+package com.suishenyun.springbootshiro.controller;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class AccountInfoController {
+
+    @RequiresRoles("admin")
+    @RequestMapping("/account-info")
+    public String home(Model model) {
+        String name = "world";
+        Subject subject = SecurityUtils.getSubject();
+        PrincipalCollection principalCollection = subject.getPrincipals();
+        if (principalCollection != null && !principalCollection.isEmpty()) {
+            name = principalCollection.getPrimaryPrincipal().toString();
+        }
+        model.addAttribute("name",name);
+        return "account-info";
+    }
+}
